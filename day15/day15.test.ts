4905bdd8e732db5b158eb0dd0908b1963c62e917
Johan Levin <johan13@gmail.com>
2020-12-28 18:32:29 +0100

    Skip slow examples

--- day15/day15.test.ts
+++ day15/day15.test.ts
@@ -10,27 +10,26 @@ describe("Day 15", () => {
             assert.strictEqual(part1([1, 0, 15, 2, 10, 13]), 211);
         });
     });
-    // Part 2 is a bit slow. Skip the tests for now.
-    describe.skip("Part 2", () => {
-        it("Example [0, 3, 6] should return 175594", () => {
+    describe("Part 2", () => {
+        it.skip("Example [0, 3, 6] should return 175594", () => {
             assert.strictEqual(part2([0, 3, 6]), 175594);
         });
-        it("Example [1, 3, 2] should return 2578", () => {
+        it.skip("Example [1, 3, 2] should return 2578", () => {
             assert.strictEqual(part2([1, 3, 2]), 2578);
         });
-        it("Example [2, 1, 3] should return 3544142", () => {
+        it.skip("Example [2, 1, 3] should return 3544142", () => {
             assert.strictEqual(part2([2, 1, 3]), 3544142);
         });
-        it("Example [1, 2, 3] should return 261214", () => {
+        it.skip("Example [1, 2, 3] should return 261214", () => {
             assert.strictEqual(part2([1, 2, 3]), 261214);
         });
-        it("Example [2, 3, 1] should return 6895259", () => {
+        it.skip("Example [2, 3, 1] should return 6895259", () => {
             assert.strictEqual(part2([2, 3, 1]), 6895259);
         });
-        it("Example [3, 2, 1] should return 18", () => {
+        it.skip("Example [3, 2, 1] should return 18", () => {
             assert.strictEqual(part2([3, 2, 1]), 18);
         });
-        it("Example [3, 1, 2] should return 362", () => {
+        it.skip("Example [3, 1, 2] should return 362", () => {
             assert.strictEqual(part2([3, 1, 2]), 362);
         });
         it("Input should return 2159626", () => {
